5a3f6d30e39f643fb46f0395346530370c42a28d
Yana <yana.novikova2018@inbox.ru>
2024-11-21 15:12:38 +0700

    Added logic for button good

--- src/test1/Assets/Scripts/UI/Menu.cs
+++ src/test1/Assets/Scripts/UI/Menu.cs
@@ -1,17 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
     [SerializeField]
     private TMP_Text txt_Time;
 
-    private float time;
+    [SerializeField]
+    private Button btn_Good;
+
+    private void Awake()
+    {
+        btn_Good.onClick.AddListener(OnBtnGoodClick);
+    }
+
+    private void OnDestroy()
+    {
+        btn_Good.onClick.RemoveListener(OnBtnGoodClick);
+    }
 
     private void Update()
     {
         txt_Time.text = $"Time: {(int)Time.time}";
+    }
+
+    private void OnBtnGoodClick()
+    {
+        Debug.Log("That lesson was really GOOD!!");
     }
 }
