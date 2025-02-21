becb6f7f5b36473748100540943b3d389920edae
Yana <yana.novikova2018@inbox.ru>
2025-02-21 20:34:04 +0700

    Added logic for TestTube

--- src/Exp/Assets/Scripts/TestTube.cs
+++ src/Exp/Assets/Scripts/TestTube.cs
@@ -5,4 +5,21 @@ using UnityEngine;
 /// </summary>
 public class TestTube : MonoBehaviour
 {
+    [SerializeField]
+    private Transform liquid;
+
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float liquidAmount = 0.0f;
+
+    private void FixedUpdate()
+    {
+        if (liquid.localScale.y != liquidAmount)
+        {
+            Vector3 scale = liquid.localScale;
+            scale.y = liquidAmount;
+            liquid.localScale = scale;
+            liquid.gameObject.SetActive(liquidAmount != 0.0f);
+        }
+    }
 }
