2bfaf26722b7a85f7664cc1655faf6dea2f55388
Yana <yana.novikova2018@inbox.ru>
2025-02-22 21:01:06 +0700

    Added logic to move bubbles border

--- src/Exp/Assets/Scripts/TestTube.cs
+++ src/Exp/Assets/Scripts/TestTube.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 /// </summary>
 public class TestTube : MonoBehaviour
 {
+    private const float MaxBubbleBorderHeight = 0.2f;
+    private const float MinBubbleBorderHeight = 0f;
+
     [SerializeField]
     private Transform liquid;
+
+    [SerializeField]
+    private Transform bubbleBorder;
 
     [SerializeField]
     [Range(0.0f, 1.0f)]
@@ -20,6 +26,11 @@ public class TestTube : MonoBehaviour
             scale.y = liquidAmount;
             liquid.localScale = scale;
             liquid.gameObject.SetActive(liquidAmount != 0.0f);
+
+            float bubbleBorderHeight = Mathf.Lerp(MinBubbleBorderHeight, MaxBubbleBorderHeight, liquidAmount);
+            Vector3 borderPosition = bubbleBorder.localPosition;
+            borderPosition.z = bubbleBorderHeight;
+            bubbleBorder.localPosition = borderPosition;
         }
     }
 }
