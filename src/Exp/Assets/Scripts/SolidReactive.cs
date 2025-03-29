d537be9823267064b1c0067f4c4bd170f6448db0
Yana <yana.novikova2018@inbox.ru>
2025-03-29 18:49:55 +0700

    Optimized logic with reaction power

--- src/Exp/Assets/Scripts/SolidReactive.cs
+++ src/Exp/Assets/Scripts/SolidReactive.cs
@@ -2,23 +2,11 @@ using UnityEngine;
 
 public class SolidReactive : SelectableBase
 {
+    [SerializeField]
     [Range(0.0f, 1.0f)]
     private float reactionPower = 0.0f;
 
-    private bool isInitialized = false;
-
     public float ReactionPower => reactionPower;
-
-    public void Initialize(float reactionPower)
-    {
-        if (isInitialized)
-        {
-            return;
-        }
-
-        this. reactionPower = reactionPower;
-        isInitialized = true;
-    }
 
     public override void TryCombine(ISelectable combinedObject)
     {
--- src/Exp/Assets/Scripts/SolidReactiveGroup.cs
+++ src/Exp/Assets/Scripts/SolidReactiveGroup.cs
@@ -5,15 +5,10 @@ public class SolidReactiveGroup : SelectableBase
     [SerializeField]
     private SolidReactive reactivePrefab;
 
-    [SerializeField]
-    [Range(0.0f, 1.0f)]
-    private float reactionPower = 0.0f;
-
     public SolidReactive Generate()
     {
         var reactive = Instantiate(reactivePrefab, transform);
         reactive.transform.position += Vector3.up * 0.03f;
-        reactive.Initialize(reactionPower);
         return reactive;
     }
 
